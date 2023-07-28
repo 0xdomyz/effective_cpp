a662674acd63597f58f310d21b1f826763404d73
0xdomyz <yzdominic@outlook.com>
2023-07-28 16:17:58 +1000

    udapte 15

--- more_effective/03_exceptions/15_cost.cpp
+++ more_effective/03_exceptions/15_cost.cpp
@@ -1,1 +1,22 @@
-// g++ 15_1.cpp -o 15_1 -std=c++11 && ./15_1
+// g++ 15_cost.cpp -o 15_1 -std=c++11 && ./15_1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    try
+    {
+        throw 1;
+    }
+    catch (int e)
+    {
+        cout << "int exception" << endl;
+    }
+    catch (...)
+    {
+        cout << "other exception" << endl;
+    }
+    return 0;
+}
