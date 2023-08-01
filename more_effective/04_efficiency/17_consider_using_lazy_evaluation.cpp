1250b2ff3deec6d68ecec6f602a0f0f63f8d6e35
0xdomyz <yzdominic@outlook.com>
2023-08-01 18:59:31 +1000

    update 17

--- more_effective/04_efficiency/17_consider_using_lazy_evaluation.cpp
+++ more_effective/04_efficiency/17_consider_using_lazy_evaluation.cpp
@@ -1,1 +1,11 @@
 // g++ 17_consider_using_lazy_evaluation.cpp -o 17_consider_using_lazy_evaluation && ./17_consider_using_lazy_evaluation
+
+// to avoid unnecessary copying of objects
+// to distinguish reads from writes using operator[]
+// to avoid unnecessary reads from databases
+// to avoid unnecessary numerical computations.
+
+// lazy evaluation won't save your program any work if all your
+// computations are necessary.
+// Lazy evaluation is only useful when there's a reasonable chance your
+// software will be asked to perform computations that can be avoided.
