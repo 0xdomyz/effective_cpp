e2c80efeedeac3f38f043163599c8ab0f2cba327
0xdomyz <yzdominic@outlook.com>
2023-08-03 22:07:21 +1000

    UPATE 24

--- more_effective/04_efficiency/24_understand_the_costs_of_virtual_mi_and_rtti.cpp
+++ more_effective/04_efficiency/24_understand_the_costs_of_virtual_mi_and_rtti.cpp
@@ -1,1 +1,12 @@
 // g++ 24_understand_the_costs_of_virtual_mi_and_rtti.cpp -o 24_understand_the_costs_of_virtual_mi_and_rtti && ./24_understand_the_costs_of_virtual_mi_and_rtti
+
+//                      | increases       |Increases        |Reduces
+// Feature              | Size of Objects |Per-Class Data   |Inlining
+// Virtual Functions    | Yes             |Yes              |Yes
+// Multiple Inheritance | Yes             |Yes              |No
+// Virtual Base Classes |Often            |Sometimes        |No
+// RTTI                 |No               |Yes              |No
+
+// From the point of view of efficiency,
+// however, you are unlikely to do better than the compiler
+// generated implementations by coding these features yourself
