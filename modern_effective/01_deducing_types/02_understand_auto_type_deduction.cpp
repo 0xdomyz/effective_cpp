857a8a1e83a235e7f974e9825737b0e955595353
0xdomyz <yzdominic@outlook.com>
2023-08-19 21:52:31 +1000

    add 234

--- modern_effective/01_deducing_types/02_understand_auto_type_deduction.cpp
+++ modern_effective/01_deducing_types/02_understand_auto_type_deduction.cpp
@@ -1,1 +1,8 @@
 // g++ 02_understand_auto_type_deduction.cpp -o 02_understand_auto_type_deduction && ./02_understand_auto_type_deduction
+
+// • auto type deduction is usually the same as template type deduction, but auto
+// type deduction assumes that a braced initializer represents a std::initial
+// izer_list, and template type deduction doesn’t.
+
+// • auto in a function return type or a lambda parameter implies template type
+// deduction, not auto type deduction.
--- modern_effective/01_deducing_types/04_know_how_2_view_deduced_types.cpp
+++ modern_effective/01_deducing_types/04_know_how_2_view_deduced_types.cpp
@@ -1,1 +1,6 @@
 // g++ 04_know_how_2_view_deduced_types.cpp -o 04_know_how_2_view_deduced_types && ./04_know_how_2_view_deduced_types
+
+// • Deduced types can often be seen using IDE editors, compiler error messages,
+// and the Boost TypeIndex library.
+// • The results of some tools may be neither helpful nor accurate, so an under‐
+// standing of C++’s type deduction rules remains essential.
