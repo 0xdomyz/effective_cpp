d7c3019c2fa3e8c164a621a275c237d02cb0d1ee
0xdomyz <yzdominic@outlook.com>
2023-08-20 16:51:12 +1000

    add chap 2 sums

--- modern_effective/02_auto/05_prefer_auto_2_explicit_type_declarations.cpp
+++ modern_effective/02_auto/05_prefer_auto_2_explicit_type_declarations.cpp
@@ -1,1 +1,7 @@
 // g++ 05_prefer_auto_2_explicit_type_declarations.cpp -o 05_prefer_auto_2_explicit_type_declarations && ./05_prefer_auto_2_explicit_type_declarations
+
+// auto variables must be initialized, are generally immune to type mismatches
+// that can lead to portability or efficiency problems, can ease the process of
+// refactoring, and typically require less typing than variables with explicitly
+// specified types.
+// • auto-typed variables are subject to the pitfalls described in Items 2 and 6.
--- modern_effective/02_auto/06_use_the_explicit_typed_initializer_idiom.cpp
+++ modern_effective/02_auto/06_use_the_explicit_typed_initializer_idiom.cpp
@@ -1,1 +1,6 @@
 // g++ 06_use_the_explicit_typed_initializer_idiom.cpp -o 06_use_the_explicit_typed_initializer_idiom && ./06_use_the_explicit_typed_initializer_idiom
+
+// • “Invisible” proxy types can cause auto to deduce the “wrong” type for an ini‐
+// tializing expression.
+// • The explicitly typed initializer idiom forces auto to deduce the type you want
+// it to have
