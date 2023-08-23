c86504a2e73265c0988bc79ebb9c0bf5564936ef
0xdomyz <yzdominic@outlook.com>
2023-08-23 22:04:49 +1000

    udpate 23 24

--- modern_effective/05_rvalue_ref_move_perfect_forwarding/23_understand_move_forward.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/23_understand_move_forward.cpp
@@ -1,1 +1,10 @@
 // g++ 23_understand_move_forward.cpp -o 23_understand_move_forward && ./23_understand_move_forward
+
+// • std::move performs an unconditional cast to an rvalue. In and of itself, it
+// doesn’t move anything.
+//
+// • std::forward casts its argument to an rvalue only if that argument is bound
+// to an rvalue.
+//
+// • Neither std::move nor std::forward do anything at runtime.
+//
--- modern_effective/05_rvalue_ref_move_perfect_forwarding/24_dist_uni_ref_from_rvalue_ref.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/24_dist_uni_ref_from_rvalue_ref.cpp
@@ -1,1 +1,13 @@
 // g++ 24_dist_uni_ref_from_rvalue_ref.cpp -o 24_dist_uni_ref_from_rvalue_ref && ./24_dist_uni_ref_from_rvalue_ref
+
+// • If a function template parameter has type T&& for a deduced type T, or if an
+// object is declared using auto&&, the parameter or object is a universal refer‐
+// ence.
+//
+// • If the form of the type declaration isn’t precisely type&&, or if type deduction
+// does not occur, type&& denotes an rvalue reference.
+//
+// • Universal references correspond to rvalue references if they’re initialized with
+// rvalues. They correspond to lvalue references if they’re initialized with lval‐
+// ues.
+//
