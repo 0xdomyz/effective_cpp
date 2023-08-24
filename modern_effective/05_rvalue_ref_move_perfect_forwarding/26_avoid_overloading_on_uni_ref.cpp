e67cbcb43bd5c66882ecf569765ab56414bd5bc3
0xdomyz <yzdominic@outlook.com>
2023-08-24 20:27:48 +1000

    udpate 26

--- modern_effective/05_rvalue_ref_move_perfect_forwarding/26_avoid_overloading_on_uni_ref.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/26_avoid_overloading_on_uni_ref.cpp
@@ -1,1 +1,9 @@
 // g++ 26_avoid_overloading_on_uni_ref.cpp -o 26_avoid_overloading_on_uni_ref && ./26_avoid_overloading_on_uni_ref
+
+// • Overloading on universal references almost always leads to the universal refer‐
+// ence overload being called more frequently than expected.
+//
+// • Perfect-forwarding constructors are especially problematic, because they’re
+// typically better matches than copy constructors for non-const lvalues, and
+// they can hijack derived class calls to base class copy and move constructors.
+//
