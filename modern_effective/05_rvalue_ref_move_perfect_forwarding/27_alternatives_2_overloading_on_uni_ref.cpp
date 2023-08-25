3d502e68b6f0a2ffc9292d7650e091ee7331ffe8
0xdomyz <yzdominic@outlook.com>
2023-08-25 12:02:50 +1000

    add 27, 28

--- modern_effective/05_rvalue_ref_move_perfect_forwarding/27_alternatives_2_overloading_on_uni_ref.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/27_alternatives_2_overloading_on_uni_ref.cpp
@@ -1,1 +1,27 @@
 // g++ 27_alternatives_2_overloading_on_uni_ref.cpp -o 27_alternatives_2_overloading_on_uni_ref && ./27_alternatives_2_overloading_on_uni_ref
+
+// • Alternatives to the combination of universal references and overloading
+// include the use of distinct function names, passing parameters by lvalue
+// reference-to-const, passing parameters by value,
+//
+// and using tag dispatch.
+//
+// • Constraining templates via std::enable_if permits the use of universal ref‐
+// erences and overloading together, but it controls the conditions under which
+// compilers may use the universal reference overloads.
+//
+// • Universal reference parameters often have efficiency advantages, but they typ‐
+// ically have usability disadvantages
+//
+
+// overloading resolution
+// Calls to overloaded functions are resolved by looking at all
+// the parameters of all the overloads as well as all the arguments at the call site, then
+// choosing the function with the best overall match—taking into account all parame‐
+// ter/argument combinations.
+
+// tag dispatch
+// The call to the overloaded implementation functions inside logAndAdd
+// “dispatches” the work to the correct overload by causing the proper tag object to be
+// created. Hence the name for this design: tag dispatch. It’s a standard building block of
+// template metaprogramming
--- modern_effective/05_rvalue_ref_move_perfect_forwarding/28_understand_reference_collapsing.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/28_understand_reference_collapsing.cpp
@@ -1,1 +1,13 @@
 // g++ 28_understand_reference_collapsing.cpp -o 28_understand_reference_collapsing && ./28_understand_reference_collapsing
+
+// • Reference collapsing occurs in four contexts: template instantiation, auto type
+// generation, creation and use of typedefs and alias declarations, and
+// decltype.
+//
+// • When compilers generate a reference to a reference in a reference collapsing
+// context, the result becomes a single reference. If either of the original refer‐
+// ences is an lvalue reference, the result is an lvalue reference. Otherwise it’s an
+// rvalue reference.
+//
+// • Universal references are rvalue references in contexts where type deduction
+// distinguishes lvalues from rvalues and where reference collapsing occurs.
