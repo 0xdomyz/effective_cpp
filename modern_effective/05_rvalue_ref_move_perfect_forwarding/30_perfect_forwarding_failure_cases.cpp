adbc1a35bb18388aad8a83ea4a5eb7217874e349
0xdomyz <yzdominic@outlook.com>
2023-08-27 21:20:55 +1000

    add 30

--- modern_effective/05_rvalue_ref_move_perfect_forwarding/30_perfect_forwarding_failure_cases.cpp
+++ modern_effective/05_rvalue_ref_move_perfect_forwarding/30_perfect_forwarding_failure_cases.cpp
@@ -1,1 +1,9 @@
 // g++ 30_perfect_forwarding_failure_cases.cpp -o 30_perfect_forwarding_failure_cases && ./30_perfect_forwarding_failure_cases
+
+// • Perfect forwarding fails when template type deduction fails or when it deduces
+// the wrong type.
+//
+// • The kinds of arguments that lead to perfect forwarding failure are braced ini‐
+// tializers, null pointers expressed as 0 or NULL, declaration-only integral const
+// static data members, template and overloaded function names, and bitfields.
+//
